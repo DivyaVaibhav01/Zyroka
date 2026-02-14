32b7942b639165edb0fcf38e8519b9b2a6dc6b0e
Divya Vaibhav <vaibhavsingh2337@gmail.com>
2026-02-14 22:56:56 +0530

    Add files via upload

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import { Zyroka } from './lib/Handler';
+import { Player } from './lib/module/Player';
+import Queue from './lib/module/Queue';
+import { Connectors } from "shoukaku";
+export { Zyroka as Zyroka, Player, Queue, Connectors };
