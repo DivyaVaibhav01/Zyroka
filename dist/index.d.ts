17ff8ccf455e6229c3299d7a4beb919f36e64a23
Divya Vaibhav <vaibhavsingh2337@gmail.com>
2026-02-14 22:57:35 +0530

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Zyroka } from './lib/Handler';
-import { Player } from './lib/module/Player';
-import Queue from './lib/module/Queue';
-import { Connectors } from "shoukaku";
-export { Zyroka as Zyroka, Player, Queue, Connectors };
